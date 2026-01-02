746ab3aca55a3da705315dd1e8dd05a2a3d20d5c
chinyu1991 <66812428+chinyu1991@users.noreply.github.com>
2026-01-03 03:16:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from run_flow import run_main_flow
 def main():
     # driver = edge_driver.create_driver(headless=False)
     driver = edge_driver.create_driver_chrome(headless=False)
-    run_main_flow(driver)
-    #
-    # try:
-    #     run_main_flow(driver)
-    #     # input("流程结束，按回车关闭浏览器...")
-    # finally:
-    #     input("プロセス終了...")
+    # run_main_flow(driver)
+
+    try:
+        run_main_flow(driver)
+        # input("流程结束，按回车关闭浏览器...")
+    finally:
+        input("プロセス終了...")
         # driver.quit()
 
 if __name__ == "__main__":
